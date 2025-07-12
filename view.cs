879bc8db0d3b0ffc444cb87c374beb4a71dbdeb3
sofsof23 <sofia.khan4706@gmail.com>
2025-07-12 09:43:55 +0300

    Create view.cs

---
+++ view.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class View
+{
+    public void DisplayCounter(int count)
+    {
+        Console.WriteLine($"Current Count: {count}");
+    }
+
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
